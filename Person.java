7dde8d73c21617f1b3699a302446055b33e5d1b9
engez7 <ezio.recaldini@gmail.com>
2021-11-26 13:35:33 +0100

    added Person.java

---
+++ Person.java
@@ -0,0 +1,16 @@
+public class Person {
+  //instance variables (data fields)
+  private String name;
+  private String residence;
+
+  //methods (operation fields)
+  public String getName() {
+    return name;
+  }
+  public String getResidence() {
+    return residence;
+  }
+  public void setResidence(String newResidence) {
+    residence = newResidence;
+  }
+}
